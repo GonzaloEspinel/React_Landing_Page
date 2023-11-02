941b3295b2187fd355ef25a8f1dda92194689480
Gonzalo Espinel <gonzaloespinel@outlook.com>
2023-11-01 20:33:55 -0400

    change

---
+++ src/assets/components/navegation/Narbar.jsx
@@ -0,0 +1,17 @@
+
+
+export function Navbar() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+            <a href="#callToAction">Call To Action</a>
+          </li>
+          
+        </ul>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
