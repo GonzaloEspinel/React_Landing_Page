ea4735efb3023a1364849edaac940b46958e630c
Gonzalo Espinel <gonzaloespinel@outlook.com>
2023-11-03 23:36:27 -0400

    sync

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import{Navbar} from"./assets/components/navegation/Narbar"
-export function App() {
-  
 
+
+import './index.css'
+
+function App() {
   return (
     <>
-      < Navbar/>
+
+      <div id="#container">
+        <div id="#formCard">
+          <h1>
+            Peña Oficial Miami del
+          </h1>
+          <div id="#smlogo1">
+          </div>
+          <div>
+            <form>
+              <label><h5>Nombre Completo:</h5></label>
+              <input type="text" id="name" name="name" />
+              <label><h5>e-Mail:</h5></label>
+              <input type="text" id="email" name="email" />
+              <label><h5>#de Socio</h5></label>
+              <input type="text" id="socio" name="socio" />
+            </form>
+            <p>
+              Registrese gratuitamente para concursar
+              por una camiseta del primer equipo del
+              Real Madrid C.F
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export function Calendario() {
+  return (
+    <>
+      html here
+    </>
+  )
+}
+export function Registrese() {
+  return (
+    <>
+      html here
     </>
   )
 }
